8a3917ed2f03ad42d3982c9b01b7b7c5022f9c2c
kcaiueo <kcwebapply@gmail.com>
2019-01-07 19:56:40 +0900

    modify class name

--- imemo.rb
+++ imemo.rb
@@ -2,10 +2,10 @@ require "formula"
 
 HOMEBREW_IMEMO_VERSION='0.0.1'
 
-class Httr < Formula
-  desc "save memo easily on terminal."
-  url "https://github.com/kcwebapply/imemo/archive/1.2.0.tar.gz"
+class Imemo < Formula
+  desc "Save memo easily on terminal"
   homepage "https://github.com/kcwebapply/imemo"
+  url "https://github.com/kcwebapply/imemo/archive/1.2.0.tar.gz"
   sha256 "fe443ab343e7f335dfa05b772512bc75c303812f43ba7795d7ed1b6a8ca15e70"
 
   depends_on "dep" => :build
@@ -14,18 +14,21 @@ class Httr < Formula
   def install
     print buildpath
     ENV["GOPATH"] = buildpath
-    cloud_nuke_path = buildpath/"src/github.com/kcwebapply/imemo/"
-    cloud_nuke_path.install buildpath.children
+    imemo_path = buildpath/"src/github.com/kcwebapply/imemo/"
+    imemo_path.install buildpath.children
 
-    cd cloud_nuke_path do
+    cd imemo_path do
       system "dep", "ensure", "-vendor-only"
-      system "go", "build", "-ldflags", "-X main.VERSION=#{version}"
+      system "go", "build"
       bin.install "imemo"
     end
   end
 
   test do
-    assert_match "memo saved!", shell_output("#{bin}/imemo save test ")
+    assert_match "memo saved!", shell_output("#{bin}/imemo save \"learning math\" ")
+    assert_match "1 learning math", shell_output("#{bin}/imemo all ")
+    assert_match "", shell_output("#{bin}/imemo delete 1")
+    assert_match "", shell_output("#{bin}/imemo all ")
   end
 end
 

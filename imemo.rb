7abdd869db7b3245fef0b93c2226a78951461e1f
Keishi wada <kcwebapply@gmail.com>
2019-01-12 22:53:08 +0900

    Update imemo.rb

--- imemo.rb
+++ imemo.rb
@@ -6,7 +6,7 @@ class Imemo < Formula
   desc "Save memo easily on terminal"
   homepage "https://github.com/kcwebapply/imemo"
   url "https://github.com/kcwebapply/imemo/archive/1.0.1.tar.gz"
-  sha256 "4808c514e1d0ce99c45945f63da3715b9067f1cb6a4ddf59ce94e3b83982dcf4"
+  sha256 "07f98298c3dd69b0f69a6c39179a476b42ef14ca189e788c40653d13559e627e"
 
   depends_on "dep" => :build
   depends_on "go" => :build
